37e4da6b848c0fe0a9eb148499c0fe05cbd7dfab
ogulcnbyrz <ogulcanboyraz@gmail.com>
2026-06-24 16:10:34 +0300

    Remove unnecessary line from app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(page_title="Futbol Tahmin", page_icon="⚽")
+
+st.title("⚽ Futbol Tahmin Sistemi")
+
+st.write("Günün maçları ve tahminleri burada gösterilecek.")
+
+st.subheader("Örnek Tahmin")
+
+st.write("""
+Galatasaray - Fenerbahçe
+
+1: %45
+X: %30
+2: %25
+""")
