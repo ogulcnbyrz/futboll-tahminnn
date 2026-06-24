fc28e775b04d8662302a638d982541a58eb53451
ogulcnbyrz <ogulcanboyraz@gmail.com>
2026-06-24 18:10:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ import requests
 import os
 from datetime import date
 
-st.set_page_config(page_title="Futbol Tahmin Sistemi")
-
-st.title("⚽ Futbol Tahmin Sistemi")
+st.title("TEST")
 
 API_KEY = os.getenv("API_FOOTBALL_KEY")
-st.write("API KEY:", API_KEY)
+
+st.write("API_KEY =", API_KEY)
+
 headers = {
     "x-apisports-key": API_KEY
 }
@@ -19,19 +19,9 @@ url = f"https://v3.football.api-sports.io/fixtures?date={today}"
 
 response = requests.get(url, headers=headers)
 
-if response.status_code == 200:
-    data = response.json()
+st.write("Status Code:", response.status_code)
 
-    matches = data["response"]
-
-    if len(matches) == 0:
-        st.warning("Bugün maç bulunamadı.")
-    else:
-        for match in matches[:20]:
-            home = match["teams"]["home"]["name"]
-            away = match["teams"]["away"]["name"]
-
-            st.subheader(f"{home} vs {away}")
-
-else:
-    st.error("API bağlantı hatası.")
+try:
+    st.json(response.json())
+except:
+    st.write(response.text)
